cf56d8291d726f5b212b6da74f156b40556f957d
J Bantay <jbantaydev@gmail.com>
2025-08-10 01:36:39 +0800

    chore: confirm Node, npm, and Git environment setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react', 'react-hooks', 'prettier', 'import', 'unused-imports'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  settings: { react: { version: 'detect' } },
+  ignorePatterns: ['dist', 'node_modules'],
+};
